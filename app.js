51b3df734177f5d62a4e4e74a4fa42be0cce3ca0
Frank Caron <frank@frankcaron.com>
2017-04-18 17:33:28 -0700

    Bailed junk

--- app.js
+++ app.js
@@ -256,12 +256,6 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    var str = '#somethingelse'
-    var M = /^(restaurant)$/.exec(messageText)
-    if (result[1]) {
-      //do something
-    }
-
 
     switch (messageText) {
 
